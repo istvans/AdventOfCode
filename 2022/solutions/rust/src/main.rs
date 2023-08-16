5308366c97a755ea8e3edbf6e2877df6c0c1fcf3
István Siroki <stevesiroki@gmail.com>
2023-08-16 11:06:37 +0100

    2022: basic rust command line parser

--- 2022/solutions/rust/src/main.rs
+++ 2022/solutions/rust/src/main.rs
@@ -1,3 +1,26 @@
+use clap::{Parser, ValueEnum};
+
+/// Each puzzle has two parts.
+#[derive(Clone, Debug, ValueEnum)]
+enum Part {
+    /// The first part; solving it unlocks the second part.
+    First,
+    /// The final, second part.
+    Second,
+}
+
+/// Solve the selected day's puzzle.
+#[derive(Parser)]
+struct Cli {
+    /// The day in the calendar to solve.
+    #[arg(short, long)]
+    day: u8,
+    /// Which part of the puzzle we want to solve.
+    #[arg(short, long, value_enum, default_value_t=Part::First)]
+    part: Part,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    println!("{:?} part of day {}", args.part, args.day);
 }
