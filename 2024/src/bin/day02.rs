e669caf60e8d7f56f66ec40b5630e9a5439a6f24
István Siroki <stevesiroki@gmail.com>
2024-12-08 01:18:33 +0000

    baby step

--- 2024/src/bin/day02.rs
+++ 2024/src/bin/day02.rs
@@ -1,12 +1,14 @@
 /// 2024 Day 2
-use std::path::Path;
+use std::{io::BufRead, path::Path};
 
 use raoc_2024::get_reader;
 
 /// Return the answer to the first part of the puzzle.
 fn part01(path: &Path) -> u32 {
     let reader = get_reader(path);
-
+    for line in reader.lines() {
+        println!("{:?}", line)
+    }
     0
 }
 
@@ -14,6 +16,5 @@ fn main() {
     let path = Path::new("./inputs/day02");
 
     let answer = part01(&path);
-    // TODO
-    println!("??? {}", answer);
+    println!("How many reports are safe? {}", answer);
 }
