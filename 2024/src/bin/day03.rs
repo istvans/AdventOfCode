f220032532bd86067ce2237c5efc140232c251b0
István Siroki <stevesiroki@gmail.com>
2024-12-15 20:21:16 +0000

    still WIP part 2

--- 2024/src/bin/day03.rs
+++ 2024/src/bin/day03.rs
@@ -1,6 +1,5 @@
 /// 2024 Day 3
 use regex::Regex;
-use std::iter::zip;
 use std::{io::BufRead, path::Path};
 
 use raoc_2024::get_reader;
@@ -31,18 +30,22 @@ impl Multiplication {
     }
 }
 
+fn multiply(range: &str) -> u32 {
+    let mul_pattern = Multiplication::pattern();
+    let multiplications = mul_pattern
+        .captures_iter(&range)
+        .map(|m| Multiplication::new(&m["first"], &m["second"]))
+        .collect::<Vec<Multiplication>>();
+    multiplications.iter().map(|m| m.execute()).sum::<u32>()
+}
+
 /// Return the answer to the first part of the puzzle.
 fn part01(path: &Path) -> u32 {
     let reader = get_reader(path);
     let mut result = 0;
-    let mul_pattern = Multiplication::pattern();
     for line in reader.lines() {
         let line = line.unwrap();
-        let multiplications = mul_pattern
-            .captures_iter(&line)
-            .map(|m| Multiplication::new(&m["first"], &m["second"]))
-            .collect::<Vec<Multiplication>>();
-        result += multiplications.iter().map(|m| m.execute()).sum::<u32>();
+        result += multiply(&line);
     }
     result
 }
@@ -51,7 +54,6 @@ fn part01(path: &Path) -> u32 {
 fn part02(path: &Path) -> u32 {
     let reader = get_reader(path);
     let mut result = 0;
-    let mul_pattern = Multiplication::pattern();
     for line in reader.lines() {
         let line = line.unwrap();
         let mut dos = line
@@ -64,9 +66,30 @@ fn part02(path: &Path) -> u32 {
             .match_indices("don't()")
             .map(|x| x.0)
             .collect::<Vec<usize>>();
-        println!("{}", line);
-        for (i, (do_index, dont_index)) in zip(dos, donts).enumerate() {
-            println!("{} {}", i, &line[do_index..dont_index])
+
+        if donts.is_empty() {
+            result += multiply(&line);
+        } else {
+            let mut donts_index = 0;
+            let mut dos_index = 0;
+            while (donts_index < donts.len()) && (dos_index < dos.len()) {
+                let mut do_index = dos[dos_index];
+                let dont_index = donts[donts_index];
+
+                if do_index < dont_index {
+                    result += multiply(&line[do_index..dont_index]);
+
+                    while do_index <= dont_index {
+                        dos_index += 1;
+                        if dos_index >= dos.len() {
+                            break;
+                        }
+                        do_index = dos[dos_index];
+                    }
+                }
+
+                donts_index += 1;
+            }
         }
     }
     result
