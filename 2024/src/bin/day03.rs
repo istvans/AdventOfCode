8a71b07c96584266e0dccb5ff523a31a68c56c22
István Siroki <stevesiroki@gmail.com>
2024-12-16 00:04:51 +0000

    we have some failing tests (yay!)
    our answer is too low
    a 76 mill answer is too low (fairly certain)
    a 90 mill answer is too high (if I recall correctly)

--- 2024/src/bin/day03.rs
+++ 2024/src/bin/day03.rs
@@ -50,47 +50,62 @@ fn part01(path: &Path) -> u32 {
     result
 }
 
+fn calculate_line_part02(line: &str) -> u32 {
+    let mut dos = line
+        .match_indices("do()")
+        .map(|x| x.0)
+        .collect::<Vec<usize>>();
+    // At the beginning of the program, mul instructions are enabled.
+    dos.insert(0, 0);
+    let mut donts = line
+        .match_indices("don't()")
+        .map(|x| x.0)
+        .collect::<Vec<usize>>();
+    // Make our algo work with an endstop.
+    donts.push(line.len());
+
+    println!("{:?}", &line[0..line.len()]);
+
+    let mut result = 0;
+    if donts.is_empty() {
+        result += multiply(&line);
+    } else {
+        let mut donts_index = 0;
+        let mut dos_index = 0;
+        while dos_index < dos.len() {
+            let mut do_index = dos[dos_index];
+            let dont_index = donts[donts_index];
+            println!("do:{:?} dont:{:?}", do_index, dont_index);
+
+            if do_index < dont_index {
+                result += multiply(&line[do_index..dont_index]);
+
+                while do_index <= dont_index {
+                    dos_index += 1;
+                    if dos_index >= dos.len() {
+                        break;
+                    }
+                    do_index = dos[dos_index];
+                    println!("after mul => do:{:?} dont:{:?}", do_index, dont_index);
+                }
+                donts_index += 1;
+                donts_index %= donts.len();
+            } else {
+                dos_index += 1;
+            }
+        }
+    }
+
+    result
+}
+
 /// Return the answer to the second part of the puzzle.
 fn part02(path: &Path) -> u32 {
     let reader = get_reader(path);
     let mut result = 0;
     for line in reader.lines() {
         let line = line.unwrap();
-        let mut dos = line
-            .match_indices("do()")
-            .map(|x| x.0)
-            .collect::<Vec<usize>>();
-        // At the beginning of the program, mul instructions are enabled.
-        dos.insert(0, 0);
-        let donts = line
-            .match_indices("don't()")
-            .map(|x| x.0)
-            .collect::<Vec<usize>>();
-
-        if donts.is_empty() {
-            result += multiply(&line);
-        } else {
-            let mut donts_index = 0;
-            let mut dos_index = 0;
-            while (donts_index < donts.len()) && (dos_index < dos.len()) {
-                let mut do_index = dos[dos_index];
-                let dont_index = donts[donts_index];
-
-                if do_index < dont_index {
-                    result += multiply(&line[do_index..dont_index]);
-
-                    while do_index <= dont_index {
-                        dos_index += 1;
-                        if dos_index >= dos.len() {
-                            break;
-                        }
-                        do_index = dos[dos_index];
-                    }
-                }
-
-                donts_index += 1;
-            }
-        }
+        result += calculate_line_part02(&line);
     }
     result
 }
@@ -113,4 +128,38 @@ fn main() {
          just the enabled multiplications? {}",
         answer
     );
+}
+
+//===================================================================
+//================================= Tests ===========================
+//===================================================================
+
+// Part 2 tests
+
+#[test]
+fn test_no_dos_or_donts() {
+    let result = calculate_line_part02("xyzmul(2,3)kdskdskdsmul(4,5)");
+    assert_eq!(result, (2 * 3) + (4 * 5));
+}
+
+#[test]
+fn test_dont_all() {
+    let result = calculate_line_part02("don't()xyzmul(2,3)kdskdskdsmul(4,5)");
+    assert_eq!(result, 0);
+}
+
+#[test]
+fn test_example() {
+    let result = calculate_line_part02(
+        "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))",
+    );
+    assert_eq!(result, (2 * 4) + (8 * 5));
+}
+
+#[test]
+fn test_multi_dont() {
+    let result = calculate_line_part02(
+        "do()xmul(2,4)&mul[3,7]don't()!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))",
+    );
+    assert_eq!(result, (2 * 4) + (8 * 5));
 }
